1a3dc3da35eaba6127f86acae02ff8abbd1dae9b
Abduxoliq Uzoqov <uzoqovabduxoliq@gmail.com>
2026-04-22 22:38:56 +0500

    Bulletproof fix for ValueError: Manually provide decoder_input_ids in DataCollator

--- train_whisper.py
+++ train_whisper.py
@@ -23,10 +23,25 @@ class DataCollatorSpeechSeq2SeqWithPadding:
     def __call__(self, features: List[Dict[str, Union[List[int], torch.Tensor]]]) -> Dict[str, torch.Tensor]:
         input_features = [{"input_features": feature["input_features"]} for feature in features]
         batch = self.processor.feature_extractor.pad(input_features, return_tensors="pt")
+        
         label_features = [{"input_ids": feature["labels"]} for feature in features]
         labels_batch = self.processor.tokenizer.pad(label_features, return_tensors="pt")
+        
         labels = labels_batch["input_ids"].masked_fill(labels_batch.attention_mask.ne(1), -100)
+        
+        # BOS tokenni qirqib tashlash (takrorlanmasligi uchun)
+        if (labels[:, 0] == self.processor.tokenizer.bos_token_id).all().cpu().item():
+            labels = labels[:, 1:]
+            
         batch["labels"] = labels
+        
+        # Xatolikni "temirbeton" qilib tuzatish: decoder_input_ids ni qo'lda yaratib uzatamiz
+        decoder_input_ids = labels.new_zeros(labels.shape)
+        decoder_input_ids[:, 1:] = labels[:, :-1].clone()
+        decoder_input_ids[:, 0] = self.processor.tokenizer.bos_token_id
+        decoder_input_ids.masked_fill_(decoder_input_ids == -100, self.processor.tokenizer.pad_token_id)
+        batch["decoder_input_ids"] = decoder_input_ids
+        
         return batch
 
 class WhisperUzbekManager:
